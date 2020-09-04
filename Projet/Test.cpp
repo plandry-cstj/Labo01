f02bcc3c28c4e7f30123b2821a6213dc48087cc1
Pierre-Olivier Landry <2033703@cstj.qc.ca>
2020-09-04 14:11:59 -0400

    Ajoutez des fichiers projet.

---
+++ Projet/Test.cpp
@@ -0,0 +1,13 @@
+// But : Créer un programme qui affiche un message à l'écran en sauvegardant plusieurs versions de ce programme avec git et de l'héberger sur la plateforme GitHub.
+// Auteur : Pierre-Olivier Landry
+// Date : 2020-09-04
+
+
+int main(/*Liste des paramètres séparés par une virgule le cas échéant*/)
+{
+
+
+
+
+	return 0;
+}
