40e16d533ddf06e25af533a2955e16c94bc6351c
Pierre-Olivier Landry <2033703@cstj.qc.ca>
2020-09-04 14:27:09 -0400

    Commit/Validation

--- Projet/Test.cpp
+++ Projet/Test.cpp
@@ -2,12 +2,16 @@
 // Auteur : Pierre-Olivier Landry
 // Date : 2020-09-04
 
+#include <iostream>		// Intègre le coffre à outils pour afficher è l'écran (o out output : sortie)
+						// ou pour lire sur le clavier (i in input : entrée)
 
 int main(/*Liste des paramètres séparés par une virgule le cas échéant*/)
 {
+	// Pour corriger les caractères accentués
+	setlocale(LC_ALL, "");
 
-
-
+	// Affichage du message à l'écran
+	std::cout << "Bienvenue à tous !";
 
 	return 0;
 }
